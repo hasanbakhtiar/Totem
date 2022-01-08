88e1b48aa570501121e4932e9ea2334605dfed27
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-01-08 20:48:21 +0400

    JavaScript Lesson1

---
+++ javsScript/lesson1-jsintro/script.js
@@ -0,0 +1,72 @@
+//one comment
+
+/* more line
+comment
+*/
+// document.write('Hello1');
+// document.writeln('Hello2');
+
+
+
+// alert("hello");
+// prompt('Adiniz daxil edin','Aygun');
+// window.alert('hello1');
+
+// debugger;
+// console.log("Hello1");
+// console.log("Hello2");
+// console.log("Hello4");
+// console.log("Hello5");
+// console.loqg("Hello3");
+
+
+
+
+// const text = document.querySelector('h1');
+// const button = document.querySelector('button');
+
+// const countryCapital=()=>{
+//     text.innerHTML = "Baku";
+// }
+// button.addEventListener('click', countryCapital);
+
+
+
+// Data Type (Data Tipləri)
+
+// Primitive(Sadə) Data Type
+        // string  - mətn
+        const infoString = 'Hello';
+        console.log(infoString);
+        // number  - rəqəm
+        const infoNumberOne = 5;
+        const infoNumberTwo = 5;
+        console.log(infoNumberOne+infoNumberTwo);
+        // boolean - doğru yanlış
+        const infoTrue = true;
+        const infoFalse = false;
+        // console.log(infoFalse);
+        // if(false){
+        //     console.log('This is true');
+        // }else{
+        //     console.log('This is false');
+        // }
+
+// Composite(Birləşik) Data Type
+        // array  - qovluq
+        const infoArray = ['Hello', 5, false];
+        // new Array();
+        console.log(infoArray);
+        // object - obyekt
+        const infoObj = {
+            name: "Aygun",
+            age:"19"
+        }
+        console.log(infoObj.age);
+
+
+// Special(xüsusi) Data Type
+        // undefined - tanımlanmamış
+        console.log(undefined);
+        // null      - boş
+        console.log(null);
