71fe89ff236a93016e5fbe9ecf30725120de2d55
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-01-18 21:01:51 +0400

    array loop

---
+++ javsScript/lesson4-loops/js/main.js
@@ -0,0 +1,91 @@
+// Loops - Döngülər
+
+let infoData = 1;
+// while
+// while(infoData >= 100){ //condition
+//     console.log("JavaScript"); //action
+//     // document.write("JavaScript<br>");
+//     infoData++;
+// }
+
+// do while
+// do{
+//     console.log(infoData);
+//     infoData++;
+// }while(infoData > 100);
+
+// for
+// for(let infoDataOne = 1; infoDataOne <= 100; infoDataOne++){
+//     console.log(infoDataOne);
+// }
+// for in
+
+// let infoArr = ['table', 'pen', 'mouse'];
+// // console.log(infoArr);
+// // console.log(infoArr[0]);
+
+// for( let x in infoArr){
+//     console.log(infoArr[x]);
+// }
+
+
+// for each 
+// for of 
+
+
+// Array
+// let vagzali = "vagzali.mp3";
+// let sarigelin = 'sarigelin.mp3';
+// let sarilirimBirine = 'sarilirimbirine.mp3';
+// let letItHappen = 'letItHappen.mp3';
+// let athena = 'athena.mp3';
+// let beliver = "beliver.mp3";
+
+// let musicFolder = [vagzali,sarigelin,sarilirimBirine,letItHappen,athena,beliver];
+// for(let i in musicFolder){
+//     console.log(musicFolder[i]);
+// }
+
+// console.log(musicFolder[1]);
+
+
+
+// let infoArr = [];
+// infoArr[0] = 'table';
+// infoArr[1] = 5;
+// infoArr[2] = 'pen';
+// console.log(infoArr);
+
+
+// let infoArr = ['table', 'pen',true, 5];
+// // infoArr[1] = 'board';
+// // infoArr[4] = 'board';
+// infoArr['hello'] = 'whatsthis';
+// console.log();
+
+
+// let uMPSUPLE_Array = ['table','pen',['snow','sunny'],[1,2,3,[true],4,5],'board'];  
+// // console.log(uMPSUPLE_Array[2][0]);
+// console.log(uMPSUPLE_Array[3][3][0]);
+
+
+
+// object - obyektlər
+
+// let car = {
+//     name: "Mercedes",
+//     model:'s500',
+//     color: 'red',
+//     year: '2001',
+//     engine: 'v5',
+//     otherCar:{
+//         newCar: 'lada 2103',
+//         oldCar:['qaz31','lombargini','zaz','pultdu masin']
+//     }
+// }
+
+// console.log(car.otherCar.oldCar[1]);
+
+
+
+
