4dd4719fefd8058418e090ab0ebaba3df607fb4b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-02-23 14:48:32 +0400

    jquery last

---
+++ javaScript/JQuery/Lesson3/js/script.js
@@ -0,0 +1,51 @@
+
+
+var nadir = $.noConflict();
+nadir(function(){
+    nadir(".info").click(function(){
+        nadir('.box').animate({
+    left: '250px',
+    opacity: '0.5',
+    // height: '150px',
+    // width: '150px'
+  });
+
+  var reshad = nadir.noConflict();
+  reshad(function(){
+    reshad(".info").click(function(){
+        reshad('.box').animate({
+    left: '250px',
+    opacity: '0.5',
+    // height: '150px',
+    // width: '150px'
+  });
+//   var jq = $.noConflict();
+// jq(function(){
+//   jq("button").click(function(){
+//     jq("p").text("jQuery is still working!");
+//   });
+// });
+
+//   stop()
+//   div.animate({height: '300px', opacity: '0.4'}, "2000");
+//   div.animate({width: '300px', opacity: '0.8'}, "slow");
+//   div.animate({height: '100px', opacity: '0.4'}, "slow");
+//   div.animate({width: '100px', opacity: '0.8'}, "slow");
+//  $('.box').slideToggle(2000);
+//  $('.box').fadeTo("2000", 0.15);
+//  slideDown()
+//  slideUp()
+//  slideToggle()
+ 
+
+    // $(".box").fadeToggle(2000, ()=>{
+    //     document.write("Hello1")
+    // });
+
+    // $(".box").fadeToggle(
+    //     // myself work , callback
+    // });
+    // $(".box").fadeToggle();
+    // $(".box").fadeToggle('slow');
+    });
+  });
